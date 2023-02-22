55e437ba1ba2f33148df1f4e30db559640479a40
collin-smith23 <collinmk@live.com>
2023-02-21 22:57:16 -0700

    deleted parseInt on create a spot

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -124,7 +124,7 @@ router.post('/', validSpot, handleValidationErrors ,async (req, res) => {
 
 if (user){
     const newSpot = await Spot.create({
-    ownerId: parseInt(userId),
+    ownerId: userId,
     address,
     city,
     state,

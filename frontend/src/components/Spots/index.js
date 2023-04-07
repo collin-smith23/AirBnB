e9165119147879479e755900fcbd2d5e6f829a36
collin-smith23 <collinmk@live.com>
2023-04-06 23:26:05 -0600

    fixed if spot has no image

--- frontend/src/components/Spots/index.js
+++ frontend/src/components/Spots/index.js
@@ -4,6 +4,14 @@ import './Spots.css'
 function Spots() {
     const [spots, setSpots] = useState([]);
 
+    const imgSource = (spot) => {
+        if (spot.previewImage === null){
+            spot.previewImage = 'https://imgs.search.brave.com/DBfFhJz7r65R_iKRH2jQE6oaZZo2qQYSCHJLvyiMjv0/rs:fit:474:296:1/g:ce/aHR0cDovL3d3dy5m/c3hhZGRvbnMuY29t/L3N0YXRpYy9pbWcv/bm8tcHJldmlldy5q/cGc'
+        }
+        return spot.previewImage
+    }
+
+
     useEffect(() => {
         fetch('/api/spots')
             .then(res => res.json())
@@ -12,12 +20,12 @@ function Spots() {
                 setSpots(spotsArray)
             })
     }, [])
-    
+    // console.log(spots)
     return (
         <div className="spots-slot">
     {spots.map(spot => (
         <div className="spot-slot">
-            <img className='spot-image' src={spot.imageUrl} alt={spot.name}/>
+            <img className='spot-image' src={imgSource(spot)}/>
             <div>{spot.city}, {spot.state}
                 <div title={spot.name}>
                     {spot.reviews}
